1f90e3d001016deef058878524695adbffec263a
Dmitriy Astafev <dim-as82@yandex.ru>
2023-05-19 22:25:22 +0500

    add file check before update

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -8,7 +8,7 @@ import { useDispatch, useSelector } from "react-redux";
 const Account = () => {
   const filePicker = useRef(null);
   const dispatch = useDispatch();
-  const files = useSelector((state) => state.file.files)
+  const files = useSelector((state) => state.file.files);
   const [selectedFiles, setSelectedFiles] = useState(null);
   const token = localStorage.getItem("token");
 
@@ -22,8 +22,26 @@ const Account = () => {
 
   const uploadFileHandler = (e) => {
     e.preventDefault();
+    //Проверяем, выбраны ли файлы
     if (!selectedFiles) {
       alert("Выберите файлы для загрузки");
+      return;
+    }
+    //Проверяем, чтобы общее число файлов не превышало 20
+    if (selectedFiles.length + files.length > 20) {
+      alert(
+        "Превышен лимит на общее количество загруженных файлов(не более 20)"
+      );
+      return;
+    }
+    //Считаем суммарный размер загружаемых файлов и проверяем, чтобы он был не больше 1 мегабайта
+    const filesSize = selectedFiles.reduce((result, file) => {
+      return result + file.size;
+    }, 0);
+    if (filesSize > 1048576) {
+      alert(
+        "Превышен лимит на размер загружаемых файлов(не более 1 мегабайта)"
+      );
       return;
     }
     const formData = new FormData(e.target);
@@ -70,7 +88,9 @@ const Account = () => {
           Загрузить файлы
         </Button>
       </form>
-      <Typography align="center">Всего загружено файлов: {files.length} из 20</Typography>
+      <Typography align="center">
+        Всего загружено файлов: {files.length} из 20
+      </Typography>
       <FileList />
     </Container>
   );
